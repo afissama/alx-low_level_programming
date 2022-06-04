68120a22dbfced42402e5c3236700e24eafdf227
afis <mahoutinafis@gmail.com>
2022-06-04 02:42:00 +0100

    Update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,15 +13,15 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = i+1; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
-			if (i == j )
+			if (i == j)
 				continue;
 
 			putchar('0' + (i % 10));
 			putchar('0' + (j % 10));
-			
-			if (i+j == 17)
+
+			if (i + j == 17)
 				continue;
 
 			putchar(',');
