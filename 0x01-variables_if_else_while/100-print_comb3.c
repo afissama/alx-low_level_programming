67015ddccdc84e4b3c5848aea7c8c1818da14038
afis <mahoutinafis@gmail.com>
2022-06-04 02:39:25 +0100

    100 Print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i+1; j <= 9; j++)
+		{
+			if (i == j )
+				continue;
+
+			putchar('0' + (i % 10));
+			putchar('0' + (j % 10));
+			
+			if (i+j == 17)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
