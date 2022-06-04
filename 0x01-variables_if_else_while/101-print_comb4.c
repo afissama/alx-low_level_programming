c4fa282f1d29dd8656dc2dc950931d4c5ff3772c
afis <mahoutinafis@gmail.com>
2022-06-04 02:54:29 +0100

    3 numbers combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				 putchar('0' + (i % 10));
+				 putchar('0' + (j % 10));
+				 putchar('0' + (k % 10));
+				
+				 if (i + j + k == 24)
+					 continue;
+				 putchar(',');
+				 putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
