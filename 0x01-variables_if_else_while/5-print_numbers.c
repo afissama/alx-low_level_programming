fc002dcf00686ed1f48282a1afeb6f1629d89a83
afis <mahoutinafis@gmail.com>
2022-06-04 01:03:17 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
