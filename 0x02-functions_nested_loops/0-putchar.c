e2d34fca2bd229c47056befbe9819be4071ba805
afis <mahoutinafis@gmail.com>
2022-06-07 22:26:40 +0100

    Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -6,6 +7,14 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	printf("_putchar");
+{
+	char text_[] = "_putchar";
+	int pos = 0;
+
+	for (pos = 0; pos <= 7; pos++)
+	{
+		_putchar(text_[pos]);
+	}
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "_putchar.c"
+
+#endif
