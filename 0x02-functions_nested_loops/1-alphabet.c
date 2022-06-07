4cf7a30591d4f66755d6086baba6eeca106c8785
afis <mahoutinafis@gmail.com>
2022-06-08 00:05:42 +0100

    Update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,34 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <unistd.h>
 #include "main.h"
-
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
 
 /**
  * print_alphabet - writes the alphabet
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 void print_alphabet(void)
 {
-    int i;
+	int i;
 
 	for (i = 'A'; i <= 'Z'; i++)
 	{
 		_putchar(tolower(i));
 	}
-    _putchar('\n');
+	_putchar('\n');
 }
