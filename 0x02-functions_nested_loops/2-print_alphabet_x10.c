846c1d157528831d3e67ec040555125de0beb471
afis <mahoutinafis@gmail.com>
2022-06-08 00:16:29 +0100

    Print 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - writes the alphabet
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 10 ; i++)
+	{
+		print_alphabet();
+	}
+}
+
+/**
+ * print_alphabet - writes the alphabet
+ *
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		_putchar(tolower(i));
+	}
+	_putchar('\n');
+}
