8a1831c9e879c50f6635b7eb16df43416c197f2c
afis <mahoutinafis@gmail.com>
2022-06-10 03:41:47 +0100

    Fix

--- 0x04-more_functions_nested_loops/test.c
+++ 0x04-more_functions_nested_loops/test.c
@@ -8,8 +8,9 @@
  */
 int main(void)
 {
-    print_square(2);
-    print_square(10);
-    print_square(0);
+    print_diagonal(0);
+    print_diagonal(2);
+    print_diagonal(10);
+    print_diagonal(-4);
     return (0);
 }
