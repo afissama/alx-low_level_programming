6d433e54205da328cdf36efda0fd978961f437e1
afis <mahoutinafis@gmail.com>
2022-06-18 02:59:14 +0100

    Update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,13 +13,9 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (s1[i] > s2[i])
+		if (s1[i] > s2[i] || s1[i] < s2[i])
 		{
-			return (-15);
-		}
-		if (s1[i] < s2[i])
-		{
-			return (15);
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
