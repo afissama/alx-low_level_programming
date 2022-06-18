773b1f8ab11d317b869ea4280b9832f3b332f4af
afis <mahoutinafis@gmail.com>
2022-06-18 03:07:27 +0100

    Update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,7 +20,7 @@ char *cap_string(char *str)
 		|| str[i - 1] == '!' || str[i - 1] == '?'
 		|| str[i - 1] == '"' || str[i - 1] == '('
 		|| str[i - 1] == ')' || str[i - 1] == '}'
-		|| str[i - 1] == '{')
+		|| str[i - 1] == '{' || i == 0)
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
