633747ebab789f659932e89a2b54ef195fef9cb1
afis <mahoutinafis@gmail.com>
2022-06-22 23:39:09 +0100

    Update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,16 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(s[0]);
-	if (s[1])
+	if (s[0])
 	{
-		_puts_recursion(&s[1]);
-	}
-	else
-	{
-		_putchar('\n');
+		_putchar(s[0]);
+		if (s[1])
+		{
+			_puts_recursion(&s[1]);
+		}
+		else
+		{
+			_putchar('\n');
+		}
 	}
 }
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,9 +8,12 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (s[1])
+	if (s[0])
 	{
-		_print_rev_recursion(&s[1]);
+		if (s[1])
+		{
+			_print_rev_recursion(&s[1]);
+		}
+		_putchar(s[0]);
 	}
-	_putchar(s[0]);
 }
