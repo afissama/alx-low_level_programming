764501dddcd59ae56c91ede568954b34cfbe5288
afis <mahoutinafis@gmail.com>
2022-06-22 22:15:17 +0100

    Write a function that returns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - return the length of parsing param
+ * @s: string passing pointer
+ *
+ * Return: length of s
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(&s[1]));
+
+}
