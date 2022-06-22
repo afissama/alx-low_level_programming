7b14c704e19244770a47e42f3ff52816f91b083e
afis <mahoutinafis@gmail.com>
2022-06-22 22:31:42 +0100

    Create pow function

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _pow_recursion - return x power y
+ * @x: int
+ * @y: int
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, (y - 1)));
+}
