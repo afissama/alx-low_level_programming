4d53be95187e7f3d9f7096a1b9879519ea8231a7
afis <mahoutinafis@gmail.com>
2022-06-24 13:48:47 +0100

    Number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - main Entry
+ *
+ * @argc: int
+ * @argv: char**
+ *
+ * Return: int
+ */
+int main(int argc, char const __attribute__ ((unused)) *argv[])
+{
+	_putchar((argc - 1) + '0');
+	_putchar('\n');
+	return (0);
+}
