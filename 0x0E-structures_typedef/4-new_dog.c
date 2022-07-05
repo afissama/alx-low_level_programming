430c5c13b7f65cb1964054fbd810737557ec5233
afis <mahoutinafis@gmail.com>
2022-07-06 00:08:09 +0100

    Uding typedef and redefine the struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,3 +24,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (_dog);
 }
+
+/**
+ * init_dog - init struct dog
+ *
+ * @d: struct pointer to init
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->age = age;
+		d->name = name;
+		d->owner = owner;
+	}
+}
