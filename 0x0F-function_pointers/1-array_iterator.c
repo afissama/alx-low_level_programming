19ba90fff354a6c502a69eb5837661451d3af1eb
afis <mahoutinafis@gmail.com>
2022-07-06 22:53:09 +0100

    Write a function that call another among array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * array_iterator - call action for each array value
+ *
+ * @array: array of int
+ * @size: array size
+ * @action: callback function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,5 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
