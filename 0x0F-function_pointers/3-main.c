2c6c94820aa3daefbf2d7a59e641d4d170db06d2
afis <mahoutinafis@gmail.com>
2022-07-07 02:47:54 +0100

    Mini calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry function
+ *
+ * @argc: number of arguments
+ * @argv: args passed
+ * Return: int defined success status
+ */
+int main(int argc, char *argv[])
+{
+	int num_1, num_2;
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num_1 = atoi(argv[1]);
+	num_2 = atoi(argv[3]);
+	op = get_op_func(argv[2]);
+
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((strcmp("/", argv[2]) == 0) || strcmp("%", argv[2]) == 0)
+	{
+		if (num_2 == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+	}
+
+	printf("%d\n", op(num_1, num_2));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - add a and b
+ *
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract a from b
+ *
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply a and b
+ *
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide a per b
+ *
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder or div a per b
+ *
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
