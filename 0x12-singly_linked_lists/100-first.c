c1107dc68c5a50a567bb415580e901c3ef13a7a6
afis <mahoutinafis@gmail.com>
2022-07-16 02:54:37 +0100

    Print text before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include<stdio.h>
+
+/**
+ * _beforemain - print some text before the main
+ *
+ */
+void _beforemain(void)
+{
+	printf("You're beat! and yet, you ");
+	printf("must allow,\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -1,3 +1,6 @@
 #Prints list
 #list nodes
 #add to head
+#add to the end
+#free a list
+#print before main
