57c88eae3ada8eaefd83b7482472c620d055a51d
afis <mahoutinafis@gmail.com>
2022-07-16 03:48:56 +0100

    Hello worl in assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,23 @@
-section .data
-msg: DB 'Hello, Holberton', 10
-msgSize EQU $ - msg
-
-global start
-
-section .text
-    mov rax, 4
-    mov rbx, 1
-    mov rcx, msg
-    mov rdx, msgSize
-    int 0x80
-    mov rax, 1
-    mov rbx, 0
-    int 0x80
-    ret
+    ; ##########################
+    ; exporing function hello_world
+    ; ##########################
+    global main
+    ; ##########################
+    ; code
+    ; ##########################
+    section .text
+main: 
+    ; ##########################
+    ; write(1, message, length)
+    ; ##########################
+    mov  rax, 1              ; system call for write
+    mov  rdi, 1              ; making file handle stdout
+    mov  rsi, message        ; passing adress of string to output
+    mov  rdx, length         ; number of bytes
+    syscall                  ; invoking os to write
+    ; ##########################
+    ; Variables
+    ; ##########################
+    section .data   
+message: db "Hello, Holberton", 0xa ; const char * message = "Hello, Holberton "\n"
+length: equ $-message;  int length= len(message) =12; 
