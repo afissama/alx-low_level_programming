ec3a2af026fc5676b1bd1089a49802efcdd19876
afis <mahoutinafis@gmail.com>
2022-07-20 00:33:44 +0100

    Update insert node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * insert_nodeint_at_index - insert node at index
+ *
+ * @head: list head
+ * @idx: index
+ * @n: param for new node
+ * Return: listint_t*
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t **head_tmp, *new_node, *temp_next;
