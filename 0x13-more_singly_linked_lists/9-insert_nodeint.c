d43fc5e272c5a33c5c654e97221847ebb6f79b12
afis <mahoutinafis@gmail.com>
2022-07-19 23:27:54 +0100

    Add at index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t **head_tmp, *new_node, *temp_next;
+
+	head_tmp = head;
+	while (idx--)
+	{
+		if ((*head_tmp) == NULL)
+			return (NULL);
+
+		head_tmp = &((*head_tmp)->next);
+	}
+	new_node = malloc(sizeof(listint_t *));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	temp_next = (*head_tmp);
+	*head_tmp = new_node;
+	new_node->next = temp_next;
+
+	return (new_node);
+}
