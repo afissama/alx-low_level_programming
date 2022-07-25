bf25986d1814bffffdeb74bcd8f4336fde83f9ee
afis <mahoutinafis@gmail.com>
2022-07-25 17:40:20 +0100

    Flip n to m

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,46 @@
+int nb_digits_in_bin(unsigned long int n);
+
+/**
+ * flip_bits - Get the number of bit to flip to m from n
+ *
+ * @n: unsigned long int
+ * @m: unsigned long int
+ * Return: unsigned int
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor_;
+	unsigned int count;
+	int xor_len, i;
+
+	xor_ = (n ^ m);
+	xor_len = nb_digits_in_bin(xor_);
+	count = 0;
+	for (i = 0; i < xor_len; i++)
+	{
+		if ((xor_ & (1 << (i))))
+		{
+			count++;
+		}
+	}
+	return (count);
+}
+
+/**
+ * nb_digits_in_bin - get the number of n binary digits
+ *
+ * @n: unsigned long int
+ * Return: the number of n digit in binary (int)
+ */
+int nb_digits_in_bin(unsigned long int n)
+{
+	int count, temp_n;
+
+	count = 0;
+	temp_n = n;
+	do {
+		temp_n = n >> (++count);
+	} while (temp_n != 0);
+
+	return (count);
+}
