852f8ac50a49a02c4018a1098526220e86fe26d3
afis <mahoutinafis@gmail.com>
2022-07-25 17:10:31 +0100

    Clear bit at index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * clear_bit -  Set bit to 0 at index
+ *
+ * @n: unsigned long int
+ * @index: index to check
+ * Return: 1 or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL)
+	{
+		return (-1);
+	}
+
+	*n = (*n & (~(1 << (index))));
+	return (1);
+}
