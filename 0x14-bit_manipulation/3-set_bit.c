77245a72dcb89d16c0be03b1ce3696caa399ff45
afis <mahoutinafis@gmail.com>
2022-07-25 06:59:41 +0100

    Set bit to 1 at index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * set_bit -  Set bit to 1 at index
+ *
+ * @n: unsigned long int
+ * @index: index to check
+ * Return: 1 or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL)
+	{
+		return (-1);
+	}
+
+	*n = (*n | (1 << (index)));
+	return (1);
+}
