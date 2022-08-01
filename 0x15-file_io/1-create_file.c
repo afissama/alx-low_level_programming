8c87ea3f5056f0639fa13c7dfcb034e130d6b560
afis <mahoutinafis@gmail.com>
2022-08-01 16:46:03 +0100

    Fix betty style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename != NULL)
 	{
-		fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT , 0600);
+		fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 		if (fd != -1)
 		{
 			if ((write(fd, text_content, _strlen_recursion(text_content))) != -1)
