1aa3887b4ceebbdeb21161d564c6b3c8d0dd9dd3
afis <mahoutinafis@gmail.com>
2022-09-02 03:50:29 +0100

    Update

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
  * delete_dnodeint_at_index - delete nodeint at index
- * 
- * @head: root node of list 
- * @index: index of pos 
- * Return: int 
+ *
+ * @head: root node of list
+ * @index: index of pos
+ * Return: int
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
