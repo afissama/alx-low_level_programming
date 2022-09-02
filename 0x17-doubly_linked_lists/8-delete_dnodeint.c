a5c69562007ee0e376706fff484088e0d7cd8a24
afis <mahoutinafis@gmail.com>
2022-09-02 03:48:34 +0100

    Update

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - delete nodeint at index
+ * 
+ * @head: root node of list 
+ * @index: index of pos 
+ * Return: int 
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t **tmp;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	tmp = head;
+	while (index--)
+	{
+		if (*tmp == NULL)
+			return (-1);
+		tmp = &(*tmp)->next;
+	}
+	if (*tmp == NULL)
+		return (-1);
+	(*tmp) = (*tmp)->next;
+	return (1);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -9,7 +9,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ *
  */
 typedef struct dlistint_s
 {
@@ -25,4 +25,5 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 #endif
