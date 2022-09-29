d170c4b1536a3742991c7be8016fd0fe8adb2c79
afis <mahoutinafis@gmail.com>
2022-09-29 02:06:34 +0100

    get index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,13 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * @brief 
- * 
- * @param key 
- * @param size 
- * @return unsigned long int 
+ * key_index - return index of hashmap based on its key
+ *
+ * @key: the key
+ * @size: has map size
+ * Return: unsigned long int
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key) / size);
+	return (hash_djb2(key) % size);
 }
