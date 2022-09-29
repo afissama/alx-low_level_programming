8c99278b0368010bc4a4ef4f1a61f2b88768b652
afis <mahoutinafis@gmail.com>
2022-09-29 01:21:30 +0100

    Create hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,12 +10,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashtable;
 
-	hashtable = malloc(sizeof(hash_table_t *));
+	hashtable = malloc(sizeof(hash_table_t));
 	if (hashtable == NULL)
 		return (NULL);
 
 	hashtable->size = size;
-	hashtable->array = malloc(sizeof(hash_node_t **) * size);
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
 	if (hashtable->array == NULL)
 		return (NULL);
 
