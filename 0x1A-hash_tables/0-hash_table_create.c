f0c9af90c5318c19da225d97d8e715afe5c0a15f
afis <mahoutinafis@gmail.com>
2022-09-29 01:08:54 +0100

    Create hashtable

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create an hash_table
+ * 
+ * @size: hashtable size 
+ * Return: hash_table_t* 
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable;
+
+	hashtable = malloc(sizeof(hash_table_t *));
+	if (hashtable == NULL)
+		return (NULL);
+
+	hashtable->size = size;
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
+	if (hashtable->array == NULL)
+		return (NULL);
+	
+	return (hashtable);
+}
